e49a6ad0d15161bbc93a7c466b77e39142f770ae
ethanloinguyen <146493940+ethanloinguyen@users.noreply.github.com>
2026-05-27 21:36:17 -0700

    Added <Suspense> boundary so Next.js can statically render the shell of the page while deferring the useSearchParams() call to the client

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,11 +1,15 @@
 // Frontend/src/app/page.tsx
 "use client"
 
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/src/app/context/AuthContext";
 import AuthForm  from "@/components/ui/Authform";
 
 export default function Page() {
-  return <AuthForm />;
+  return (
+    <Suspense>
+      <AuthForm />
+    </Suspense>
+  );
 }
