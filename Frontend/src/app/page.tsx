ed10cbcbe7a6894c3e400a7a6e24adb3a00c67c8
Wang Ng <wshng@ucdavis.edu>
2026-05-28 15:53:17 -0700

    resolve next.JS error bug
    permission error & firebase error gone

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,10 +1,8 @@
 // Frontend/src/app/page.tsx
 "use client"
 
-import { useEffect, Suspense } from "react";
-import { useRouter } from "next/navigation";
-import { useAuth } from "@/src/app/context/AuthContext";
-import AuthForm  from "@/components/ui/Authform";
+import { Suspense } from "react";
+import AuthForm from "@/components/ui/Authform";
 
 export default function Page() {
   return (
